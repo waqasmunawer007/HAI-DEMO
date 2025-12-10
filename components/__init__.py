347531fc5ca0467ab8db1e640b8cb0f219cd8917
waqasmunawer007 <waqasmunawer007@gmail.com>
2025-12-10 11:04:06 +0500

    fix path issue

--- components/__init__.py
+++ components/__init__.py
@@ -3,6 +3,5 @@ Components package for HAI Facilities Dashboard
 Contains reusable UI components for data visualization
 """
 
-from .statistics_tree import render_statistics_tree
-
-__all__ = ['render_statistics_tree']
+# Import statements removed - import directly from submodules instead
+# Example: from components.statistics_tree import render_statistics_tree
